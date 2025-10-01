d2cc03b7b8dda08cebc24fd102c2db5e8fef1ea1
AFlow-ascii <AFiorini@qubicaamf.com>
2025-10-01 11:16:40 +0200

    Initial Hello World

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+int numero = 1;
+double numerovirgola = 2.5;
+string nome = "Alessandro";
+char carattere = 'b';
+int[] ints = [1, 2, 3, 4, 5];
+Dictionary<string, int> nome_anni = new Dictionary<string, int>
+{
+    {"Alessandro", 17},
+    {"Zheng", 18}
+};
+Console.WriteLine(numero);
+Console.WriteLine(numerovirgola);
+Console.WriteLine(nome);
+Console.WriteLine(carattere);
+Console.WriteLine(ints);
+Console.WriteLine(nome_anni);
+
